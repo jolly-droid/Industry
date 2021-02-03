77c6b3a7dff6a7aebc1e49791950fc888abca71a
julia <julia.autengruber@yahoo.com>
2021-02-03 16:52:29 +0100

    started on pipe method

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ static void forkMe(int id){
         case -1:
             fprintf(stderr, "fork went wrong %s\n", strerror(errno));
             exit(EXIT_FAILURE);
-
         case 0:
             fprintf(stdout,"this is the child-id: %d\n", id);
             break;
@@ -24,13 +23,35 @@ static void forkMe(int id){
 }
 
 static void forkMeWithPipes(int id){
+    int fd [2];
+    if(pipe(fd) == -1){
+        fprintf(stderr, "piping went wrong: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if(id == -1) exit(EXIT_FAILURE);
+    if(id == 0) { //child process
+        close(fd[0]); // close read end
+        int x;
+        printf("enter a number: \n");
+        scanf("%d", &x);
+        if (write(fd[1], &x, sizeof(int)) == -1) exit(EXIT_FAILURE);
+        close(fd[1]); // close write end
+
+    }else{ //parent process
+            close(fd[1]);
+            int y;
+            if(read(fd[0], &y, sizeof(int)) == -1) exit(EXIT_FAILURE);
+            close(fd[0]);
+            printf( "i got this number form child: %d\n", &y);
+    }
+
 
 
 }
 
 int main() {
     int id = fork();
-    forkMe(id);
-   // forkMeWithPipes(id);
+   // forkMe(id);
+    forkMeWithPipes(id);
    return 0;
 }
