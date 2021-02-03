27d00307488a843cee8c180c9a5228869494edc7
julia <julia.autengruber@yahoo.com>
2021-02-03 16:08:35 +0100

    resolved warnings

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <fcntl.h>
+
 
 //this method forks the process
 static void forkMe(int id){
@@ -19,7 +12,7 @@ static void forkMe(int id){
         case -1:
             fprintf(stderr, "fork went wrong %s\n", strerror(errno));
             exit(EXIT_FAILURE);
-            break;
+
         case 0:
             fprintf(stdout,"this is the child-id: %d\n", id);
             break;
@@ -37,8 +30,7 @@ static void forkMeWithPipes(int id){
 
 int main() {
     int id = fork();
-
-
     forkMe(id);
+   // forkMeWithPipes(id);
    return 0;
 }
