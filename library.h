b90770bb71fcd33a6af1c1ae488beea0708bd739
julia <julia.autengruber@yahoo.com>
2021-02-08 10:32:41 +0100

    moved fork to libraries

---
+++ library.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+#ifndef INDUSTRY_LIBRARY_H
+#define INDUSTRY_LIBRARY_H
+
+#endif //INDUSTRY_LIBRARY_H
+
+//first small function for forking
+void forkMe(int id);
+
+//forking this time with pipes
+void forkMeWithPipes(int id);
+
+//an example from real life with pipes
+void forkAndSum(int id, int arr[]);
+
+//another forking test function
+void forkAgain(int id);
+
+//trying to implement an exercise
+// @return returns the sum of the two given numbers
+int intmul(int a, int b, int id);
